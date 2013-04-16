11a707275ec96d836f446ea628df8b2eee12465e
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-04-16 20:58:47 +0200

    some small improvements

--- core/parse.py
+++ core/parse.py
@@ -2,6 +2,23 @@ import ast
 from utils.astpp import dump
 
 
+class NestedFrame(object):
+    pass
+
+
+class NestedLambda(NestedFrame):
+    pass
+
+
+class NestedFunction(NestedFrame):
+    def __init__(self, funcname):
+        self.funcname = funcname
+
+
+class NestedClass(NestedFrame):
+    pass
+
+
 class Identifier(ast.NodeVisitor):
     """Identifies Sources, Sinks, and Sanitizers."""
 
@@ -17,8 +34,8 @@ class Identifier(ast.NodeVisitor):
         # taint data
         self.taint = {}
 
-        # current function
-        self.curfunc = None
+        # nested frames
+        self.frames = []
 
         # errors
         self.errors = []
@@ -35,13 +52,17 @@ class Identifier(ast.NodeVisitor):
             self.g[asname] = node.module + '.' + alias.name
 
     def visit_FunctionDef(self, node):
-        self.generic_visit(node)
-        if len(node.decorator_list) == 1 and \
+        self.frames.append(NestedFunction(node.name))
+
+        if not len(self.frames) and len(node.decorator_list) == 1 and \
                 isinstance(node.decorator_list[0], ast.Call) and \
                 node.decorator_list[0].func.id == 'route':
-            self.handlers[node.decorator_list[0].args[0].s] = node
+            uri = node.decorator_list[0].args[0].s
+            method = 'GET'  # node.decorator_list[0].get('method', 'GET')
+            self.handlers[uri, method] = node
 
-        self.curfunc = node.name
+        self.generic_visit(node)
+        self.frames.pop()
 
     def visit_Attribute(self, node):
         self.generic_visit(node)
@@ -53,10 +74,12 @@ class Identifier(ast.NodeVisitor):
 
     def visit_BinOp(self, node):
         self.generic_visit(node)
-        if isinstance(node.op, ast.Mod) and \
-                isinstance(node.left, ast.Str):
+        # 'fmt' % args
+        if isinstance(node.op, ast.Mod) and isinstance(node.left, ast.Str):
+            # 'fmt' % arg
             if isinstance(node.right, ast.Attribute):
                 self.taint[node] = self.taint[node.right]
+            # 'fmt' % (args,)
             elif isinstance(node.right, ast.Tuple):
                 for el in node.right.elts:
                     self.taint[node] = \
--- thesis.py
+++ thesis.py
@@ -3,6 +3,10 @@ from utils.astpp import dump
 import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'Usage: python %s <website.py>' % sys.argv[0]
+        exit(1)
+
     root = parse(sys.argv[1])
     x = Identifier()
     x.visit(root)
