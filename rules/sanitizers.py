68f37991828524eae5508227e44dc810ac3c6088
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-05-07 15:51:50 +0200

    bottle html-escape sanitizer

---
+++ rules/sanitizers.py
@@ -0,0 +1,35 @@
+from core.taint import AttributeTaint, CallableTaint, Taint
+from rules.base import Base
+
+
+class Sanitizer(Base):
+    """Base Class for all Sanitizers."""
+
+
+class SimpleSanitizer(CallableTaint, Sanitizer):
+    """Class for defining simple sanitizers.
+
+    A simple sanitizer takes source input and emits legitimate sink output.
+
+    """
+    def __init__(self, taint_level, framework=None, version=None):
+        Sanitizer.__init__(self, framework, version)
+        CallableTaint.__init__(self, taint_level)
+
+    def call(self, arg):
+        ret = Taint(arg.taint.taint_level & ~self.taint_level)
+        print 'sani', arg.taint, self.taint_level, ret
+        return ret
+
+
+class _BottleRules(AttributeTaint, Sanitizer):
+    def __init__(self):
+        """Rules for the Bottle framework."""
+        Sanitizer.__init__(self, 'bottle', None)
+        AttributeTaint.__init__(self, -1)
+        self['html_escape'] = SimpleSanitizer(Sanitizer.XSS)
+
+
+sanitizers = {
+    'bottle': _BottleRules(),
+}
